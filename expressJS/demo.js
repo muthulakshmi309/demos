2f5dee1576b21ee338f8300c7991e400184ead9b
muthulakshmi.a <muthulakshmi.arumugam@zeuslearning.com>
2018-10-10 13:28:37 +0530

    load default index file on launch

--- expressJS/demo.js
+++ expressJS/demo.js
@@ -1,8 +1,11 @@
 var express = require("express"),
-    bodyParser = require("body-parser");
-app = express();
+    bodyParser = require("body-parser"),
+    path = require('path'),
+    app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+app.set('view engine', 'html');
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({
@@ -10,14 +13,24 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     console.log("There is some error...");
 });
-app.get("/", (req, res) => res.send("Server has started... It is running on port " + port));
-app.get("/first", (req, res) => {   
-    console.log(req.baseUrl);
-    res.send("first page is loaded");  
+app.get("/", (req, res) => res.redirect('/index'));
+
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname + '\\index.html'));
+
+    //or set the root folder
+    // res.sendFile('index.html', {
+    //     root: __dirname
+    // });
 });
+app.get("/first", (req, res) => {
+    console.log(res);
+    res.send(`first page is loaded`);
+});
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
